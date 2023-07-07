0cb38459b5eafcdf103d1268d142f42483a9f468
Danil Menkin <danil.crypton@blinkfinance.xyz>
2023-07-07 14:48:30 +0700

    fix value for register tx

--- src/register/index.ts
+++ src/register/index.ts
@@ -3,6 +3,7 @@ import { calculateRiskFactor } from './marginEngine'
 
 async function main() {
   const user = '0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc'
+  const value = parseEther('10.000000000000000000').toHexString()
 
   const ETH = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
   const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
@@ -11,7 +12,7 @@ async function main() {
     [
       {
         token: ETH,
-        amount: parseEther('10.000000000000000000').toHexString(),
+        amount: value,
       },
     ],
     {
@@ -25,7 +26,7 @@ async function main() {
   console.log(
     JSON.stringify({
       from: user,
-      value: 0,
+      value,
       request: { for: user, ...response },
     }),
   )
