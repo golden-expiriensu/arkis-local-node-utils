5a542b8a59fb6ea2675bbfb195e4958468ff61d3
Danil Menkin <danil.crypton@blinkfinance.xyz>
2023-08-09 16:53:17 +0400

    refactor

--- src/close/index.ts
+++ src/close/index.ts
@@ -2,6 +2,7 @@ import account from '../account.json'
 import { getAddress } from '../utils'
 import { getFactoryAddress } from '../provider'
 import { Contract, constants } from 'ethers'
+import { parseEther } from 'ethers/lib/utils'
 
 export async function closeMarginAccount(
   factory: Contract,
@@ -14,26 +15,7 @@ export async function closeMarginAccount(
         protocol: '',
         destination: 'localhost',
       },
-      content: [
-        {
-          sequence: [0],
-          increasePositionInstructions: [],
-          decreasePositionInstructions: [],
-          exchangeInstructions: [
-            {
-              protocol: 'transfer',
-              request: {
-                path: `${account.leverage.token}000000000000000000000000000000000000000000000000`,
-                amountIn: onlySuspend ? constants.MaxUint256 : 1,
-                extraData: '0x',
-                minAmountOut: 0,
-                recipient: await getFactoryAddress(),
-              },
-            },
-          ],
-          exchangeAllInstructions: [],
-        },
-      ],
+      content: await createContent(onlySuspend),
     },
     onComplete: [],
   }
@@ -44,4 +26,27 @@ export async function closeMarginAccount(
     `Margin account ${getAddress(marginAccountBytes32)} was ${onlySuspend ? 'suspended' : 'successfully liquidated'}`,
   )
   return marginAccountBytes32
+}
+
+async function createContent(onlySuspend: boolean): Promise<Object> {
+  const envelope: any = {
+    sequence: [0],
+    increasePositionInstructions: [],
+    decreasePositionInstructions: [],
+    exchangeInstructions: [],
+    exchangeAllInstructions: [],
+  }
+
+  envelope.exchangeInstructions.push({
+    protocol: 'transfer',
+    request: {
+      path: `${account.leverage.token}000000000000000000000000000000000000000000000000`,
+      amountIn: onlySuspend ? constants.MaxUint256 : 1,
+      extraData: '0x',
+      minAmountOut: 0,
+      recipient: await getFactoryAddress(),
+    },
+  })
+
+  return [envelope]
 }
