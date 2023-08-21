2cb4643028b8d01c479f17344097157739447914
Danil Menkin <danil.crypton@blinkfinance.xyz>
2023-08-21 12:20:34 +0400

    improved logging

--- src/utils/treasure.ts
+++ src/utils/treasure.ts
@@ -1,6 +1,6 @@
 import { BigNumber, Contract } from 'ethers'
 
-import { parseEther } from 'ethers/lib/utils'
+import { formatEther, formatUnits, parseEther } from 'ethers/lib/utils'
 import { MaybeTx, WalletLike } from 'src/types'
 import { getProvider } from 'src/utils'
 
@@ -8,7 +8,8 @@ export class Treasure {
   constructor(private wallet: WalletLike) {}
 
   async topUpTokenBalance(token: Contract, account: string, upToAmount: string | BigNumber): Promise<MaybeTx> {
-    console.log(`Topping up ${token.address} token balance up to amount ${upToAmount}`)
+    const decimals = Number(await token.decimals())
+    console.log(`Topping up ${await token.symbol()} balance up to amount ${formatUnits(upToAmount, decimals)}`)
 
     const target = BigNumber.from(upToAmount)
     const currentBalance = await token.balanceOf(account)
@@ -18,7 +19,7 @@ export class Treasure {
   }
 
   async topUpEthBalance(account: string, upToAmount: string | BigNumber, includeGas = true): Promise<MaybeTx> {
-    console.log(`Topping up ETH balance up to amount ${upToAmount}${includeGas ? ' + gas expenses' : ''}`)
+    console.log(`Topping up ETH balance up to amount ${formatEther(upToAmount)}${includeGas ? ' + gas expenses' : ''}`)
 
     let target = BigNumber.from(upToAmount)
     if (includeGas) target = target.add(parseEther('1'))
