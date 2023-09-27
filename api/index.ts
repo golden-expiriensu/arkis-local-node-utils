750ea4fa83e112c889eb6ddb0ad691cd98d073e1
Kumar Parmar (Hi-Tech Talents LLC) <v-kumparmar@microsoft.com>
2023-09-27 10:04:52 +0300

    body parser for express app

--- api/index.ts
+++ api/index.ts
@@ -8,10 +8,15 @@ import { registerMarginAccount as register } from './register'
 import { Asset, Scenario } from './types'
 import { getFactory, getJsonProvider, getOwner } from './utils'
 import { ImpersonatedSigner } from './utils/impersonatedSigner'
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express()
 
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
 
 app.get('/ping', (_, res) => {
   res.send('pong')
