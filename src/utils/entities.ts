a1bee58b92e85bf64672db5abf14fb7052bf580f
Danil Menkin <danil.crypton@blinkfinance.xyz>
2023-09-27 11:12:20 +0400

    changed margin engine entity

--- src/utils/entities.ts
+++ src/utils/entities.ts
@@ -12,10 +12,8 @@ export async function getFactory(): Promise<Contract> {
 
 export async function getOwner(): Promise<WalletLike> {
   return new HighBandwidthWallet({
-    mnemonic: {
-      phrase: 'test test test test test test test test test test test junk',
-      path: `m/44'/60'/0'/0/4`,
-    },
+    privateKey:
+      process.env.MARGIN_ENGINE_PRIVATE_KEY ?? '0xd2b4a376e35f9a7719a237cc4ec4e644fd875017a978479e17c9833e16117651',
   }).sync()
 }
 
