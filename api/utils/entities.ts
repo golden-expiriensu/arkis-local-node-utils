b7e12b37c4d8a3b2ddc5332d6964d14aa020964c
Danil Menkin <danil.crypton@blinkfinance.xyz>
2023-09-27 16:17:56 +0400

    log addresses

--- api/utils/entities.ts
+++ api/utils/entities.ts
@@ -11,10 +11,12 @@ export async function getFactory(): Promise<Contract> {
 }
 
 export async function getOwner(): Promise<WalletLike> {
-  return new HighBandwidthWallet({
+  const result = await new HighBandwidthWallet({
     privateKey:
       process.env.MARGIN_ENGINE_PRIVATE_KEY ?? '0xd2b4a376e35f9a7719a237cc4ec4e644fd875017a978479e17c9833e16117651',
   }).sync()
+  console.log(`owner address: ${result.address}`)
+  return result
 }
 
 export async function getFactoryAddress(): Promise<string> {
