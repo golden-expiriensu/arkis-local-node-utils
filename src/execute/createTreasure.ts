919b250f0a5c099879201794b5c45a99efced1bf
Danil Menkin <danil.crypton@blinkfinance.xyz>
2023-09-27 12:23:15 +0400

    allow to specify treasure address

--- src/execute/createTreasure.ts
+++ src/execute/createTreasure.ts
@@ -3,10 +3,8 @@ import { HighBandwidthWallet, Treasure } from 'src/utils'
 export async function createTreasure(): Promise<Treasure> {
   return new Treasure(
     await new HighBandwidthWallet({
-      mnemonic: {
-        phrase: 'test test test test test test test test test test test junk',
-        path: `m/44'/60'/0'/0/0`,
-      },
+      privateKey:
+        process.env.TREASURE_PRIVATE_KEY ?? '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
     }).sync(),
   )
 }
