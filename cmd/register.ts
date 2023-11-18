3a2f7c3c4becbf554d30155644205a1916b0d472
Danil Menkin <danil.menkin@arkis.xyz>
2023-11-18 12:43:40 +0300

    removed old register file

--- cmd/register.ts
+++
@@ -1,5 +0,0 @@
-import { Asset } from "../types";
-
-export async function register(owner: string, leverage: Asset, collateral: Asset[]): Promise<void> {
-  console.log(`Registering account for ${owner}: ${JSON.stringify({ leverage, collateral })}`)
-}
